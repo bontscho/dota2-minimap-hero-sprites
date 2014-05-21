940fbba47edb1129af31159437a9a3aa04286b7f
bontscho <software@bontscho.de>
2014-05-21 21:55:16 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<link rel="stylesheet"
+      type="text/css"
+      href="https://github.com/bontscho/dota2-minimap-hero-sprites/master/assets/stylesheets/dota2minimapheroes.css"
+/>
+# README
+
+This is a CSS spritesheet for Dota 2 Minimap Hero Icons.
+
+## Demo & Usage
+
+<i class="d2mh nyx"></i>
+<i class="d2mh npc_dota_hero_antimage"></i>
+<i class="d2mh hero-5"></i>
+
+## Installation
+
+
+## Update Workflow
+
+1. Update `src/heroes.json` with latest json output from `http://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1?language=en_us&key=<your steam web api key>`
+2. Extract `materials/vgui/hud/minimap_hero_sheet.vtf` from Dota 2 pak files and export to `assets/images/minimap_hero_sheet.png`
+3. Extract `scripts/mod_textures.txt` from Dota 2 pak files to `src/mod_textures.txt`
+
+
+### Tools
+
+e.g. VPK extract, VTFEdit
+
---
+++ generate.js
@@ -0,0 +1,52 @@
+var vdf     = require('vdf'),
+    fs      = require('fs'),
+    swig    = require('swig');
+
+// read mod textfile
+var mod_textures    = vdf.parse(fs.readFileSync(__dirname + "/src/mod_textures.txt", "utf-8"));
+var heroes_file     = JSON.parse(fs.readFileSync(__dirname + "/src/heroes.json"))["result"]["heroes"];
+
+// make heroes a hash
+var heroes = {};
+heroes_file.forEach(function(obj) {
+   heroes[obj["name"]] = obj;
+});
+
+// icons object from hash, keys might change on future updates
+var icons = mod_textures["sprites/640_hud"]["TextureData"];
+
+var heroIcons = [];
+
+for(key in icons) {
+    // pick only icons from hero sheet file, this could be extended in the future
+    if(icons[key]["file"] != "vgui/hud/minimap_hero_sheet")  {
+        continue;
+    }
+    var hero_name = key.substring(17)
+
+    // this considers brewmaster spirits and arcana items
+    if(typeof(heroes[hero_name]) == "undefined") {
+        heroes[hero_name] = { id: null, name_full: hero_name, name: hero_name.substring(14) }
+    }
+
+    var data = {
+        id:         heroes[hero_name]["id"],
+        name:       hero_name,
+        name_full:  heroes[hero_name]["name"],
+        width:      icons[key]["width"],
+        height:     icons[key]["height"],
+        x:          icons[key]["x"],
+        y:          icons[key]["y"]
+    }
+
+    heroIcons.push(data);
+};
+
+var template = swig.compileFile(__dirname + "/src/dota2minimapheroes.css.swig");
+var output = template({
+    heroes: heroIcons
+});
+
+fs.writeFileSync("assets/stylesheets/dota2minimapheroes.css", output);
+
+console.log("CSS file generated");
